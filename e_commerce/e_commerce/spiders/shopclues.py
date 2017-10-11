8ea033134397c301b08b911be9d5eb4d0a53b664
Jasneet Singh Sawhney <aashitsingh2010@gmail.com>
2017-10-12 04:06:55 +0530

    more spiders

---
+++ e_commerce/e_commerce/spiders/shopclues.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class ShopcluesSpider(scrapy.Spider):
+    name = "shopclues"
+    allowed_domains = ["www.shopclues.com/mobiles-featured-store-4g-smartphone.html"]
+    start_urls = ['http://www.shopclues.com/mobiles-featured-store-4g-smartphone.html/']
+
+    def parse(self, response):
+        #Extracting the content using css selectors
+        img = response.css("img::attr(data-img)").extract()
+        name = response.css("img::attr(title)").extract()
+        price = response.css(".p_price").extract()
+        discount = response.css(".prd_discount").extract()
+       
+        #Give the extracted content row wise
+        for item in zip(img,name, price, discount):
+            #create a dictionary to store the scraped info
+            scraped_info = {
+                'img' : item[0],
+                'name' : item[1],
+                'price' : item[2],
+                'discount' : item[3]
+            }
+
+            #yield or give the scraped info to scrapy
+            yield scraped_info        
